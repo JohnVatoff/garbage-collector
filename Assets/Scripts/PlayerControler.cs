03f8a80619fc0a597ca2054b98630ec30c2ef8c7
logix <vatovi@abv.bg>
2017-01-20 20:37:56 +0200

    PlayScene

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.AddForce(transform.forward * 100);
+            Debug.Log("add force");
+        }
+	}
+}
